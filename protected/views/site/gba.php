9054889003856fe66eafe8fb1b4ae14b45cfe201
Javier Cabezas <jcleyton@gmail.com>
2014-11-04 21:05:13 -0300

    Ajax button instead of span to call th ajaxExcecuteCommand method

--- protected/views/site/gba.php
+++ protected/views/site/gba.php
@@ -1,16 +1,53 @@
+<?php 
+/** 
+ * GBA KEY -> KEYBOARD KEY
+ *	a 		-> z
+ * 	b 		-> x
+ *	L 		-> l
+ *	R 		-> r
+ *	start 	-> m
+ *	select  -> n
+ *	up 		-> w
+ *	down 	-> s
+ *	left 	-> a
+ *	right 	-> d
+ *	speed   -> This is a special case and we send "speed" to the script.
+ */
+
+?>
 <div class="content">
  	<div class="left">
-		<p class="button" id="z"> a </p>
-		<p> &nbsp; </p>
-		<p class="button" id="x"> b </p>
-		<p> &nbsp; </p>
-		<p class="button" id="l"> L </p>
-		<p> &nbsp; </p>
-		<p class="button" id="r"> R </p>
-		<p> &nbsp; </p>
-		<p class="button" id="m"> start </p>
-		<p> &nbsp; </p>
-		<p class="button" id="n"> select </p>
+ 	
+ 		<?php
+		echo CHtml::ajaxSubmitButton('a',Yii::app()->createUrl('site/ajaxExcecuteCommand'),
+        	array('type'=>'POST','data'=> 'js:{"key": "z" }'), array('class'=>'small_btn' ));
+		?>
+
+		<?php
+		echo CHtml::ajaxSubmitButton('b',Yii::app()->createUrl('site/ajaxExcecuteCommand'),
+        	array('type'=>'POST','data'=> 'js:{"key": "x" }'), array('class'=>'small_btn' ));
+		?>
+
+		<?php
+		echo CHtml::ajaxSubmitButton('L',Yii::app()->createUrl('site/ajaxExcecuteCommand'),
+        	array('type'=>'POST','data'=> 'js:{"key": "l" }'), array('class'=>'small_btn' ));
+		?>
+
+		<?php
+		echo CHtml::ajaxSubmitButton('R',Yii::app()->createUrl('site/ajaxExcecuteCommand'),
+        	array('type'=>'POST','data'=> 'js:{"key": "r" }'), array('class'=>'small_btn' ));
+		?>
+		
+		<?php
+		echo CHtml::ajaxSubmitButton('start',Yii::app()->createUrl('site/ajaxExcecuteCommand'),
+        	array('type'=>'POST','data'=> 'js:{"key": "m" }'), array('class'=>'small_btn' ));
+		?>
+		
+		<?php
+		echo CHtml::ajaxSubmitButton('select',Yii::app()->createUrl('site/ajaxExcecuteCommand'),
+        	array('type'=>'POST','data'=> 'js:{"key": "n" }'), array('class'=>'small_btn' ));
+		?>
+
 	</div>
   	<div class="right">
 		<p class="button" id="s"> v </p>
